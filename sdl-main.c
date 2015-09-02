33954ead27293634a58b3da6d5552540ab66efde
Michael Weber <mtweber@gmail.com>
2015-09-02 13:10:45 +0000

    Add hardware/software rendering test.

--- sdl-main.c
+++ sdl-main.c
@@ -12,6 +12,11 @@
 #include <cairo/cairo.h>
 #include <cairo/cairo-gl.h>
 #include <SDL2/SDL.h>
+#include "cairosdl.h"
+
+SDL_Window *window;
+SDL_GLContext glcontext;
+SDL_Surface* screen;
 
 #ifdef _WIN32
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -20,58 +25,65 @@ int main(int argc, char** argv)
 #endif
 {
 	double prevt, cpuTime;
-	SDL_Window *window;
-	SDL_GLContext glcontext;
 	SDL_Event event;
 	int done = 0, i;
+	SDL_Surface* screen;
 	cairo_device_t *device;
 	cairo_t *cr;
 	cairo_surface_t *surface;
+	int opengl = 0;
 
 	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-
-	window = SDL_CreateWindow("Test SDL 2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		1300, 800, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-
-	glcontext = SDL_GL_CreateContext(window);
-	if(!glcontext)
+	if(argc > 1 && strcmp(argv[1], "opengl") == 0)
 	{
-		printf("got error %s\n", SDL_GetError());
-		return 1;
-	}
+		printf("OPENGL mode\n");
+		opengl = 1;
 
-	if(SDL_GL_MakeCurrent(window, glcontext) < 0)
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+		window = SDL_CreateWindow("Test SDL 2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			1600, 900, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+		glcontext = SDL_GL_CreateContext(window);
+		if(!glcontext)
+		{
+			printf("got error %s\n", SDL_GetError());
+			return 1;
+		}
+
+		if(SDL_GL_MakeCurrent(window, glcontext) < 0)
+		{
+			printf("OpenGL context could not be made current! SDL Error: %s\n", SDL_GetError());
+			return 1;
+		}
+
+		device = cairo_sdl_device_create(window);
+		surface = cairo_sdl_surface_create(device, 1600, 900);
+		cr = cairo_create(surface);
+		SDL_GL_SetSwapInterval(0);
+	}
+	else
 	{
-		printf("OpenGL context could not be made current! SDL Error: %s\n", SDL_GetError());
-		return 1;
+		printf("SOFTWARE mode\n");
+		opengl = 0;
+
+		window = SDL_CreateWindow("Test SDL 2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			1600, 900, SDL_WINDOW_SHOWN);
+
+		screen = SDL_GetWindowSurface(window);
 	}
-
-printf("%s\n", glGetString(GL_VERSION));
-
-printf("a\n");
-	device = cairo_sdl_device_create(window);
-printf("b\n");
-	surface = cairo_sdl_surface_create(device, 1300, 800);
-printf("c\n");
-	cr = cairo_create(surface);
-printf("d\n");
-
-	SDL_GL_SetSwapInterval(0);
 
 	prevt = 0;
 	while(!done)
 	{
 		int x = 10, y = 10;
         int winWidth, winHeight;
-        int fbWidth, fbHeight;
-        float pxRatio;
 		int start = SDL_GetTicks(), end;
 
         while(SDL_PollEvent(&event))
@@ -83,22 +95,26 @@ printf("d\n");
 		}
 
 		SDL_GetWindowSize(window, &winWidth, &winHeight);
-		SDL_GetWindowSize(window, &fbWidth, &fbHeight);
-        // Calculate pixel ration for hi-dpi devices.
-        pxRatio = (float)fbWidth / (float)winWidth;
 
-        // Update and render
-        glViewport(0, 0, winWidth, winHeight);
-        glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+		if(opengl)
+		{
+        	glViewport(0, 0, winWidth, winHeight);
+        	glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
+        	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+		}
+		else
+		{
+			SDL_LockSurface(screen);
+			cr = cairosdl_create(screen);
+		}
 
+		cairo_set_source_rgb(cr, 1.0, 0.0, 0.0);
 		for(i = 0; i < 15000; i++)
 		{
-			cairo_set_source_rgb(cr, 1.0, 0.0, 0.0);
-			cairo_rectangle(cr, x, y, 8, 8);
+			cairo_rectangle(cr, x, y, 6, 6);
 			cairo_fill(cr);
 
-			x += 10;
+			x += 8;
 			if(x >= winWidth - 10)
 			{
 				x = 10;
@@ -106,14 +122,28 @@ printf("d\n");
 			}
 		}
 
-		cairo_gl_surface_swapbuffers(surface);
-		end = SDL_GetTicks();
+		if(opengl)
+		{
+			cairo_gl_surface_swapbuffers(surface);
+			SDL_UpdateWindowSurface(window);
+		}
+		else
+		{
+			cairosdl_destroy(cr);
+			SDL_UnlockSurface(screen);
+			SDL_UpdateWindowSurface(window);
+		}
 
+		end = SDL_GetTicks();
 		printf("%f, %d\n", 1000.0 / (end - start), end - start);
 	}
 
-	cairo_surface_destroy(surface);
-	cairo_device_destroy(device);
+	if(opengl)
+	{
+		cairo_surface_destroy(surface);
+		cairo_device_destroy(device);
+	}
+
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
